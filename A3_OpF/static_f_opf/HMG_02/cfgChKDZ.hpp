d1c90b474efcd023b3521981e901c61ab091237f
Senicluxus <senicluxus@gmail.com>
2021-06-01 14:09:14 -0400

    Major Bug Fixes
    Removed Raven. Removed leftover helmets that were ported. Removed cut faction leftovers (turrets).

--- A3_OpF/static_f_opf/HMG_02/cfgChKDZ.hpp
+++ A3_OpF/static_f_opf/HMG_02/cfgChKDZ.hpp
@@ -79,25 +79,3 @@ class Opf_O_S_HMG_02_high_F: HMG_02_high_base_F
 		};
 	};
 };
-
-/* Independent */
-class Opf_I_S_HMG_02_F: Opf_O_S_HMG_02_F
-{
-    author = $STR_A3_A_AveryTheKitty;
-    //editorPreview = "\A3_Opf\EditorPreviews_F_Opf\Data\CfgVehicles\Opf_I_S_HMG_02_F.jpg";
-    scope = public;
-    scopeCurator = public;
-    side = TGuerrila;
-    faction = Opf_IND_S_F;
-	crew = Opf_I_S_Soldier_1_F;
-};
-class Opf_I_S_HMG_02_high_F: Opf_O_S_HMG_02_high_F
-{
-    author = $STR_A3_A_AveryTheKitty;
-    //editorPreview = "\A3_Opf\EditorPreviews_F_Opf\Data\CfgVehicles\Opf_I_S_HMG_02_high_F.jpg";
-    scope = public;
-    scopeCurator = public;
-    side = TGuerrila;
-    faction = Opf_IND_S_F;
-	crew = Opf_I_S_Soldier_1_F;
-};
